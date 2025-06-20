b3fb59f518ba1a8c1eaf2ca6d8712feaa1353e84
Kaustubh Patil <patilkaustubh990@gmail.com>
2025-06-20 22:49:24 +0530

    feat: refactor Header component layout and improve authentication button handling

--- components/header.jsx
+++ components/header.jsx
@@ -1,18 +1,40 @@
 import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "./ui/button"
+import { PenBoxIcon } from "lucide-react"
 
 
 const Header = () => {
-  return (
-    <div>
-        <SignedOut>
-            <SignInButton/>
-        </SignedOut>
+    return (
+        <header className="container mx-auto">
+            <nav className="py-6 px-4 flex justify-between items-center">
+                <Link href="/">
+                    <h1 className="text-2xl text-white font-extrabold">Workflow Manager⚙️🛠️</h1>
+                </Link>
 
-        <SignedIn>
-            <UserButton/>
-        </SignedIn>
-    </div>
-  )
+            <div className="flex items-center gap-4">
+                <Link href="/project/create">
+                    <Button variant="destructive" className="flex items-center gap-2">
+                    <PenBoxIcon size={19} />
+                        <span>Create Project</span>
+                    </Button>
+                </Link>               
+
+                <SignedOut>
+                    <SignInButton forceRedirectUrl="/onboarding">
+                    <Button>Login</Button>
+                    </SignInButton>
+                </SignedOut>
+
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+            </nav>
+            
+        </header>
+    )
 }
 
 export default Header
